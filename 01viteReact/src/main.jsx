52df625b2f5a4927859302d40a55c04f5447f9a2
LazySukanta <dindasukanta19@gmail.com>
2024-03-05 22:21:29 +0530

    custom react practice

--- 01viteReact/src/main.jsx
+++ 01viteReact/src/main.jsx
@@ -2,8 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom App</h1>
+    </div>
+  )
+}
+
+const anotherUser = "Chai or react"
+
+const ReactElement = React.createElement(
+  'a',
+  {href: "https://google.com",target:"_blank"},
+  'CLICK FOR GOOGLE',
+  anotherUser
+
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    // <App />
+    // <MyApp/>
+    // MyApp()
+    ReactElement
 )
