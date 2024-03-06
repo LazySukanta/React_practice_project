6def4fe3f0c07c3934bcf40c4587df7cfd4ee7dd
LazySukanta <dindasukanta19@gmail.com>
2024-03-06 17:30:06 +0530

    counter using useState hook

--- 02Counter/src/App.jsx
+++ 02Counter/src/App.jsx
@@ -1,22 +1,29 @@
+import { useState } from "react";
+
+// let counter = 0;
 
 function App() {
-  let value = 2
-  const addValue =  () => {
-    console.log("Clicked");
-    value = value+1
-  }
+  let [counter, setCounter] = useState(10);
+
+  const addValue = () => {
+    if (counter >= 0 && counter < 20) {
+      setCounter(counter + 1);
+    }
+  };
+  const removeValue = () => {
+    if (counter >= 1 && counter <= 20) {
+      setCounter(counter - 1);
+    }
+  };
 
   return (
     <>
       <h1>Counter</h1>
-      <h2>Counter value : {value}</h2>
-      <button
-        onClick={ addValue }
-      
-      >Add value</button>
-      <button>Remove value</button>
+      <h2>Counter value : {counter}</h2>
+      <button onClick={addValue}>Add value</button>
+      <button onClick={removeValue}>Remove value</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
