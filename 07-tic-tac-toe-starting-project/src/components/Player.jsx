3a9b6929f72a07262bf09ef7b5e72bb462cc81a1
LazySukanta <dindasukanta19@gmail.com>
2024-04-05 13:36:40 +0530

    Till player jsx

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,16 +1,33 @@
 import { useState } from "react"
 
-const Player = ({ name, symbol }) => {
-  const [isEditing, setEditing] = useState(false)
+const Player = ({ initialName, symbol }) => {
+  const [isEditing, setIsEditing] = useState(false)
+  const [playerName, setPlayerName] = useState(initialName)
+
+  function handleEditClick() {
+    setIsEditing((prev) => !prev)
+  }
+  function handleChange(e) {
+    setPlayerName(e.target.value)
+  }
 
   return (
     <li>
       <span className="player">
-        {isEditing ? <input type="text" /> : <span className="player-name">{name}</span>}
+        {
+          isEditing ?
+            <input
+              Value={playerName}
+              type="text"
+              required
+              onChange={handleChange}
+            /> :
+            <span className="player-name">{playerName}</span>
+        }
 
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={() => setEditing((prev) => !prev)} >{isEditing ? "Save" : "Edit"}</button>
+      <button onClick={handleEditClick} >{isEditing ? "Save" : "Edit"}</button>
     </li>
   )
 }
