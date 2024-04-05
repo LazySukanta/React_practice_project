bb3b08f1953305f8ee6129885433bf7afb6e1f04
Sukanta <dindasukanta19@gmail.com>
2024-04-05 08:17:26 +0530

    tic tac toe initial

---
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -0,0 +1,28 @@
+
+function App() {
+
+
+  return (
+    <main>
+      <div id="game-container">
+        {/* Players */}
+        <ol id="players">
+          <li>
+            <span className="player-name">Player1</span>
+            <span className="player-symbol">X</span>
+          </li>
+          <li>
+            <span className="player-name">Player2</span>
+            <span className="player-symbol">O</span>
+          </li>
+
+        </ol>
+
+
+        Game Board
+      </div>
+    </main>
+  )
+}
+
+export default App
