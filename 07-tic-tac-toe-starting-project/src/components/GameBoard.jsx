7d278ae6f0764dc4313fd128827019cf9cacdd4e
LazySukanta <dindasukanta19@gmail.com>
2024-04-07 12:50:30 +0530

    completed

--- 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,10 +1,5 @@
 
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null]
-]
-const GameBoard = ({ onSelectSquare, turns }) => {
+const GameBoard = ({ onSelectSquare, board }) => {
   // const [gameBoard, setGameBoard] = useState(initialGameBoard)
   // function handleSelect(rowIndex, colIndex) {
   //   setGameBoard((prevGameBoard) => {
@@ -15,22 +10,17 @@ const GameBoard = ({ onSelectSquare, turns }) => {
   //   //toggling the active player
   //   onSelectSquare();
   // }
-  let gameBoard = initialGameBoard
-  for (const turn of turns) {
-    const { square, player } = turn
-    const {row,col} = square
-    gameBoard[row][col] = player
-  }
+
 
   return (
     <ol id="game-board">
-      {gameBoard.map((row, rowIndex) =>
+      {board.map((row, rowIndex) =>
         <li key={rowIndex} >
           <ol>
             {
               row.map((playerSymbol, colIndex) =>
                 <li key={colIndex}>
-                  <button onClick={() => onSelectSquare(rowIndex,colIndex)}>{playerSymbol}</button>
+                  <button onClick={() => onSelectSquare(rowIndex, colIndex)} disabled={playerSymbol !== null} >{playerSymbol}</button>
                 </li>
               )
             }
