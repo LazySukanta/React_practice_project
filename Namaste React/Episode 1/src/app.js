8de3c33e8f4316784011dc4f3447d362bebe40b5
Sukanta <dindasukanta19@gmail.com>
2024-04-24 09:24:51 +0530

    react router

--- Namaste React/Episode 1/src/app.js
+++ Namaste React/Episode 1/src/app.js
@@ -2,16 +2,31 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./component/Header";
 import Body from "./component/Body";
-
+import { createBrowserRouter,RouterProvider } from "react-router-dom";
+import About from "./component/About";
 
 const AppLayout = () => {
   return (
     <div className="app">
       <Header />
-      <Body />
+      <Body /> 
     </div>
   );
 };
+const appRoter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+  },
+  {
+    path: "/about",
+    element:<About />
+  },
+  {
+    path: "/contact",
+    element:<About />
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRoter} />);
